22f4c030e192a596f53316a916e5b4bf128e715a
Rajkiran Singh Mashanwale <g.rajksingh@gmail.com>
2025-09-23 19:16:47 +0200

    updating scripts with minor changes

---
+++ scripts/fetch_crypto.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Session
+
+from backend.services.db_services import get_db
+from utilities.get_crypto_rates import fetch_cryptocurrency_data
+from utilities.update_investments import get_data_from_investments
+
+db: Session = next(get_db())
+# Get all investment data and stock prices upfront
+all_investment_data = get_data_from_investments(db)
+crypto_coin_list = all_investment_data['crypto']
+fetch_cryptocurrency_data(crypto_coin_list)
