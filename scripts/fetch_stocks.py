342b83ccc4ef7bc2839d46c1c4d6f0c2d78bb411
Rajkiran Singh Mashanwale <g.rajksingh@gmail.com>
2025-10-01 18:24:07 +0200

    fixed all stock updaters

--- scripts/fetch_stocks.py
+++ scripts/fetch_stocks.py
@@ -1,32 +1,30 @@
-import logging
 import sys
 
 from sqlalchemy.orm import Session
 
 from backend.services.db_services import get_db
-from backend.services.investments.dividend import get_all_dividends
 from utilities.stock_price_fetcher import StockPriceFetcher as sPF, stockFetcher
-from utilities.update_investments import get_data_from_investments
+from utilities.update_investments import get_data_from_investments, get_dividends_data
 
 # Add the app root directory to Python path
 sys.path.insert(0, '/app')
 
-# Initialize logging
-logger = logging.getLogger(__name__)
-
 db: Session = next(get_db())
 # Get all investment data and stock prices upfront
 all_investment_data = get_data_from_investments(db)
-# Get All the dividends data
-dividends_data = get_all_dividends(db)
+dividends_data = get_dividends_data(db)
 
 # Extracting investment specific data from the DB
 common_stock_list = all_investment_data.get("common_stocks", {})
 dividend_stock_list = all_investment_data.get("stocks_with_dividends", {})
 
-# Get the latest prices and NAVs
-logger.info(f"Common Stock List: {common_stock_list}")
-logger.info(f"Dividend Stock List: {dividend_stock_list}")
-
-# common_stock_prices = get_stock_prices_in_bulk(common_stock_list)
+# Get the latest stock prices
+common_stock_prices = sPF.get_stock_prices_in_bulk(stockFetcher, common_stock_list)
 dividend_stock_prices = sPF.get_stock_prices_in_bulk(stockFetcher, dividend_stock_list)
+
+# update the investment and summary
+sPF.update_stock_investments(stockFetcher, db, common_stock_prices)
+sPF.update_stock_investments(stockFetcher, db, dividend_stock_prices, dividends_data)
+
+sPF.update_stock_summary(stockFetcher, db, common_stock_prices)
+sPF.update_stock_summary(stockFetcher, db, dividend_stock_prices, dividends_data)
